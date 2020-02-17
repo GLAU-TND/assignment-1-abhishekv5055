1b723b57450dc619209854753a6163b33bf7a1d7
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-02-17 23:09:57 +0530

    create all getter and setter

--- src/contact/Contact.java
+++ src/contact/Contact.java
@@ -28,12 +28,42 @@ public class Contact {
                 "-------- * -------- * -------- * --------");
     }
 
-    public Contact(String firstName, String lastName, String email, ArrayList phoneNumber) {
+    public Contact(String firstName, String lastName, String email, ArrayList<Integer> phoneNumber) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phoneNumber = phoneNumber;
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
 
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public ArrayList<Integer> getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(ArrayList<Integer> phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
 }
