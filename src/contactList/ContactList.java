802000fc67b3d52e3f3d6b5ac71c0e804dc0280c
abhishekv5055 <abhishek.vaish_int@gla.ac.in>
2020-02-17 22:21:27 +0530

    create a generic class

---
+++ src/contactList/ContactList.java
@@ -0,0 +1,12 @@
+/*
+ *   Created by IntelliJ IDEA Ultimate, 2020
+ *   User: Abhishek
+ *   Date: 17-02-2020
+ *   Time: 10:19 PM
+ */
+
+
+package contactList;
+
+public class ContactList<Contact> {
+}
